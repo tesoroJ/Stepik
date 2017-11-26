607270f471badb6a4d865d6fb0f6db2fe060c2db
Григорий Гук <grigorijguk@Grigorijs-MacBook-Pro.local>
2017-11-25 18:07:43 -0800

    1

--- TSPserver.py
+++ TSPserver.py
@@ -8,11 +8,22 @@ while True:
     conn, addr = s.accept()
     child_pid = os.fork()
     if child_pid == 0:
-        rec = conn.recv(1024)
-        conn.send(rec)
+        data = conn.recv(1024)
+        conn.send(data)
         conn.close()
         sys.exit()
     else:
         conn.close()
 
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('127.0.0.1', 1234))
+s.listen(10)
+pid = os.fork()
+while True:
+    conn, addr = s.accept()
+    data = conn.recv(1024)
+    conn.send(data)
+    conn.close()
 
